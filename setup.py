f3516dfa02ba83496d2914dd01e4b3471d76810a
Jonas Hagen <jonas.hagen3@gmail.com>
2018-10-28 12:40:00 +0100

    Minimal setup and package.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ofcourse",
+    version="1.0.0",
+    url="",
+    author="Jonas Hagen",
+    author_email="jonas.hagen3@gmail.com",
+    description="Tools to manage a course.",
+    packages=find_packages(),
+    install_requires=["wheel", "pyyaml"],
+)
