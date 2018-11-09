fff563cfb6fc11d622987abf2bc1eb1dc0036c54
Jonas Hagen <jonas.hagen3@gmail.com>
2018-11-09 23:34:39 +0100

    Parsers.

---
+++ ofcourse/parsers.py
@@ -0,0 +1,27 @@
+from ruamel import yaml
+from ofcourse import models
+
+
+def contacts_to_list(contacts):
+    lst = list()
+    for channel, adresses in contacts.items():
+        if not isinstance(adresses, (list, tuple)):
+            adresses = [adresses]
+        for i, a in enumerate(adresses):
+            lst.append(models.Contact(channel=channel, address=a, order=i))
+    return lst
+
+
+def person_list_parser(f):
+    y = yaml.YAML()
+    lst = list()
+    for name, p_dict in y.load(f).items():
+        if "contact" in p_dict:
+            p_dict["contact"] = contacts_to_list(p_dict["contact"])
+        if "first_name" not in p_dict:
+            p_dict["first_name"] = name.split(" ")[0]
+        if "last_name" not in p_dict:
+            p_dict["last_name"] = name.split(" ")[-1]
+        lst.append(models.Person(**p_dict))
+
+    return lst
