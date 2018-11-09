04221f12e8d2bf12463e92c205b6391b3db9a992
Jonas Hagen <jonas.hagen3@gmail.com>
2018-11-09 22:11:49 +0100

    Person and Course model.

--- ofcourse/models.py
+++ ofcourse/models.py
@@ -1,10 +1,8 @@
 from dataclasses import dataclass
+from dataclasses import field
+import datetime
+import typing
 import phonenumbers
-
-
-@dataclass
-class Person:
-    pass
 
 
 @dataclass
@@ -23,12 +21,50 @@ class Contact:
         if self.is_phone:
             phone = phonenumbers.parse(self.address, "CH")
             if not phonenumbers.is_valid_number(phone):
-                raise ValueError("invalid phone number " + self.address)
+                raise ValueError("Invalid phone number " + self.address)
             self.address = phonenumbers.format_number(
                 phone, phonenumbers.PhoneNumberFormat.INTERNATIONAL
             )
 
 
 @dataclass
+class Person:
+    first_name: str
+    last_name: str
+    adress: str = field(repr=False, default="")
+    city: str = field(default="")
+    plz: str = field(repr=False, default="")
+    birthdate: datetime.date = field(default=None)
+    gender: str = field(default="")
+    contact: typing.List[Contact] = field(repr=False, default_factory=list)
+    notes: typing.List[str] = field(repr=False, default_factory=list)
+
+    def __post_init__(self):
+        self.normalize()
+
+    @property
+    def full_name(self):
+        return self.first_name + " " + self.last_name
+
+    def normalize(self):
+        if self.gender:
+            if self.gender not in ("o", "f", "m"):
+                raise ValueError("Unknown gender " + self.gender)
+        if self.plz:
+            try:
+                if "-" in self.plz:
+                    int(self.plz.split("-")[1])
+                else:
+                    int(self.plz)
+            except ValueError as e:
+                raise ValueError("Invalid plz code " + self.plz)
+
+
+@dataclass
 class Course:
-    pass
+    title: str
+    start: datetime.date = field(default=None)
+    end: datetime.date = field(default=None)
+    number: int = field(repr=False, default=0)
+    code: str = field(default="")
+    participants: typing.List[Person] = field(repr=False, default_factory=list)
--- tests/test_models.py
+++ tests/test_models.py
@@ -6,3 +6,11 @@ from ofcourse import models
 def test_contact_phone(kind):
     c = models.Contact(kind, "0791234567")
     assert c.address == "+41 79 123 45 67"
+
+
+def test_person():
+    p = models.Person("Alice", "Alma", "Hüntzi 3", "Bern", "3007")
+
+
+def test_course():
+    c = models.Course(title='Laber Blubber')
