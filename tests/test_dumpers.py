69bedb186ca04c48ff83c87067a8167d41ee6712
Jonas Hagen <jonas.hagen3@gmail.com>
2018-11-10 00:40:23 +0100

    Course text dumper.

--- tests/test_dumpers.py
+++ tests/test_dumpers.py
@@ -18,3 +18,23 @@ def test_dump_person_list():
     lst = parsers.person_list_parser(f)
 
     assert len(lst) == 2
+
+
+def test_dump_course_text():
+    c1 = models.Contact("emergency", "0774568293")
+    c2 = models.Contact("mobile", "0791234567")
+    c3 = models.Contact("email", "a@b.ef")
+
+    p1 = models.Person("Aline", "Alma", contact=[c1, c2, c3])
+    p2 = models.Person("David", "Düber")
+
+    course = models.Course(title='Laber', participants=[p1, p2])
+
+    f = io.StringIO()
+    dumpers.course_text_dumper(f, course)
+    f.seek(0)
+    lines = list(f)
+
+    assert len(lines) == 7 + 2
+
+
