aec4b44e342ba2a53ac7d9625340d3b4161d3b74
Jonas Hagen <jonas.hagen3@gmail.com>
2018-11-09 22:43:57 +0100

    Channel list serializer.

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,5 +1,8 @@
 import pytest
 from ofcourse import models
+from dataclasses import asdict, fields
+from collections import OrderedDict
+import datetime
 
 
 @pytest.mark.parametrize("kind", ["mobile", "phone", "emergency"])
@@ -9,8 +12,11 @@ def test_contact_phone(kind):
 
 
 def test_person():
+    c = models.Contact("mobile", "0791234567")
     p = models.Person("Alice", "Alma", "Hüntzi 3", "Bern", "3007")
+    p.contact.append(c)
+    p.birthdate = datetime.date(2010, 1, 5)
 
 
 def test_course():
-    c = models.Course(title='Laber Blubber')
+    c = models.Course(title="Laber Blubber")
