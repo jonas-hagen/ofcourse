220a30b7df466369b50e7f49445f040fb1504b40
Jonas Hagen <jonas.hagen3@gmail.com>
2018-11-09 23:05:08 +0100

    Other serializers.

--- ofcourse/serializers.py
+++ ofcourse/serializers.py
@@ -1,17 +1,33 @@
 from collections import OrderedDict
+from collections import defaultdict
 from dataclasses import asdict
 from dataclasses import fields
 from ofcourse import models
 
 
 def person_list_serializer(person_list):
-    pass
+    persons = list()
+    for p in sorted(person_list, key=models.Person.key):
+        cs = contact_list_serializer(p.contact)
+        ps = asdict(p, dict_factory=OrderedDict)
+        ps["contact"] = cs
+        persons.append(ps)
+    return persons
 
 
 def contact_list_serializer(contact_list):
-    contact_list = sorted(contact_list, key=models.Contact.key)
-    return OrderedDict(((c.channel, c.address) for c in contact_list))
+    cs = defaultdict(list)
+    for c in sorted(contact_list, key=models.Contact.key):
+        cs[c.channel].append(c.address)
+    cs = OrderedDict(
+        sorted(
+            cs.items(), key=lambda c: models.Contact._get_channel_order().index(c[0])
+        )
+    )
+    return cs
 
 
 def course_serializer(course):
-    pass
+    cs = asdict(course, dict_factory=OrderedDict)
+    cs["participants"] = [p.full_name for p in course.participants]
+    return cs
--- tests/test_serializers.py
+++ tests/test_serializers.py
@@ -10,3 +10,18 @@ def test_contact_list_serializer():
     d = serializers.contact_list_serializer([c1, c2, c3])
 
     assert tuple(d.keys()) == ("mobile", "email", "emergency")
+    assert isinstance(d["mobile"], list)
+
+
+def test_person_list_serializer():
+    c1 = models.Contact("emergency", "0774568293")
+    c2 = models.Contact("mobile", "0791234567")
+    c3 = models.Contact("email", "a@b.ef")
+
+    p1 = models.Person("Aline", "Alma", contact=[c1, c2, c3])
+    p2 = models.Person("David", "Düber")
+
+    d = serializers.person_list_serializer([p2, p1])
+
+    assert d[0]["first_name"] == "Aline"
+    assert isinstance(d[0]["contact"]["mobile"], list)
