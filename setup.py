63158dcea99698339d01174a572904317bcbc9fa
Jonas Hagen <jonas.hagen@qrst.ch>
2021-03-06 17:38:38 +0100

    rename cli file

--- setup.py
+++ setup.py
@@ -2,13 +2,15 @@ from setuptools import setup, find_packages
 
 setup(
     name="ofcourse",
-    version="1.0.0",
+    version="1.0.1",
     url="",
     author="Jonas Hagen",
     author_email="jonas.hagen3@gmail.com",
     description="Tools to manage a course.",
     packages=["ofcourse"],
-    scripts=["scripts/ofc"],
+    entry_points = {
+        'console_scripts': ['ofc=ofcourse.ofc_cli:cli'],
+    },
     install_requires=[
         "wheel",
         "ruamel.yaml",
